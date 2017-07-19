568b42628349f9d91f7918d83763312285f37d9a
Jon Elmer <jon.elmer@stfc.ac.uk>
2017-07-19 14:04:23 +0100

    Assessor does 3D projection too!!

--- tests.py
+++ tests.py
@@ -15,11 +15,11 @@ class LaserTestCase(unittest.TestCase):
 
     def test_vertices_to_target_point(self):
         target = vertices_to_target([[0., 0., 0.], [0., 0., 1.], [1., 0., 1.], [1., 0., 0.]])
-        self.assertTrue(check_vectors(target['point'], [0.5, 0., 0.5]))
+        self.assertTrue(check_vectors(target.point, [0.5, 0., 0.5]))
 
     def test_vertices_to_target_normal(self):
         target = vertices_to_target([[0., 0., 0.], [0., 0., 1.], [1., 0., 1.], [1., 0., 0.]])
-        self.assertTrue(check_vectors(target['normal'], [0., 1., 0.]), msg="Normal is {}".format(target['normal']))
+        self.assertTrue(check_vectors(target.normal, [0., 1., 0.]), msg="Normal is {}".format(target.normal))
 
     def test_get_focus(self):
         l = Laser(1., 1., 5.)
@@ -28,15 +28,15 @@ class LaserTestCase(unittest.TestCase):
 
     def test_vertices_to_target_point_2(self):
         target = vertices_to_target([[0., 0., 0.], [0., 0., 1.], [1., 1., 1.], [1., 1., 0.]])
-        self.assertTrue(check_vectors(target['point'], [0.5, 0.5, 0.5]))
+        self.assertTrue(check_vectors(target.point, [0.5, 0.5, 0.5]))
 
     def test_vertices_to_target_normal_2(self):
         target = vertices_to_target([[0., 0., 0.], [0., 0., 1.], [1., 1., 1.], [1., 1., 0.]])
         normal = np.array([-1., 1., 0.])
         normal = normal / np.linalg.norm(normal)
 
-        self.assertTrue(check_vectors(target['normal'], normal),
-                        msg="Normal is {}".format(target['normal']))
+        self.assertTrue(check_vectors(target.normal, normal),
+                        msg="Normal is {}".format(target.normal))
 
     def test_get_focus_2(self):
         l = Laser(1., 1., 5.)
@@ -51,9 +51,10 @@ class LaserTestCase(unittest.TestCase):
 
     def test_interpolate_grid(self):
         laser = Laser(3., 3., 5.)
-        ass = PathAssessor(laser, trimesh.Trimesh())
-        target = {'point': (1.5, 0., 1.5), 'normal': (0., 1., 0.)}
-        grid = ass.interpolate_grid(target, 3)
+        path = PathFinder(laser, trimesh.Trimesh(), process=False)
+        ass = DeviationAssessor(path, resolution=3, process=False)
+        target = Target((1.5, 0., 1.5), (0., 1., 0.))
+        grid = ass.interpolate_grid(target)
         self.assertTrue(check_vector_list(sorted(grid), sorted([
             [0.5, 0., 0.5], [1.5, 0., 0.5], [2.5, 0., 0.5],
             [0.5, 0., 1.5], [1.5, 0., 1.5], [2.5, 0., 1.5],
